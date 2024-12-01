560b703cddf22167f75b23e551986b18439ebb9a
jequinterop <jose246495@gmail.com>
2024-11-30 22:21:37 -0300

    csp actualizado

--- seguridad/seguridad/settings.py
+++ seguridad/seguridad/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'csp.middleware.CSPMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
